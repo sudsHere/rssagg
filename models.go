e8947ad64089b63ccb45d3181b323edc8c6ecc45
Sudhanshu Kumar <sudhkuma@microsoft.com>
2024-02-25 18:56:10 +0530

    scrap feed and fetch posts for users

--- models.go
+++ models.go
@@ -58,16 +58,16 @@ type FeedFollows struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	UserID    uuid.UUID `json:"user_id"`
-	FeedID 	uuid.UUID `json:"feed_id"`
+	FeedID    uuid.UUID `json:"feed_id"`
 }
 
 func databaseFeedFollowstoFeedFollows(feedFollows database.FeedFollow) FeedFollows {
-	return FeedFollows {
-		ID: feedFollows.ID,
+	return FeedFollows{
+		ID:        feedFollows.ID,
 		CreatedAt: feedFollows.CreatedAt,
 		UpdatedAt: feedFollows.UpdatedAt,
-		UserID: feedFollows.UserID,
-		FeedID: feedFollows.FeedID,
+		UserID:    feedFollows.UserID,
+		FeedID:    feedFollows.FeedID,
 	}
 }
 
@@ -77,4 +77,40 @@ func databaseFeedsFollowstoFeedsFollows(dbFeedsFollows []database.FeedFollow) []
 		feedsFollows = append(feedsFollows, databaseFeedFollowstoFeedFollows(dbFeedFollows))
 	}
 	return feedsFollows
+}
+
+type Post struct {
+	ID          uuid.UUID `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string    `json:"title"`
+	Description *string   `json:"description"`
+	PublishedAt time.Time `josn:"published_at"`
+	Url         string    `json:"url"`
+	FeedID      uuid.UUID `json:"feed_id"`
+}
+
+func databasePostToPost(dbPost database.Post) Post {
+	var description *string
+	if dbPost.Description.Valid {
+		description = &dbPost.Description.String
+	}
+	return Post{
+		ID:          dbPost.ID,
+		CreatedAt:   dbPost.CreatedAt,
+		UpdatedAt:   dbPost.UpdatedAt,
+		Title:       dbPost.Title,
+		Description: description,
+		PublishedAt: dbPost.PublishedAt,
+		Url:         dbPost.Url,
+		FeedID:      dbPost.FeedID,
+	}
+}
+
+func databasePostsToPosts(dbPosts []database.Post) []Post {
+	posts := []Post{}
+	for _, dbPost := range dbPosts {
+		posts = append(posts, databasePostToPost(dbPost))
+	}
+	return posts
 }
